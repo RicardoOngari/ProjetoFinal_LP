96e6c9453702ce53ea3348bca491c1b73cd710c5
RicardoOngari <ricardo.ongari@gmail.com>
2026-06-03 01:37:47 -0300

    Codigo organizado

---
+++ main.py
@@ -0,0 +1,84 @@
+# biblioteca para limpar o terminal
+import os 
+# biblioteca para pausar o programa por um tempo
+import time
+ # Importa a lista de perguntas do arquivo perguntas.py
+from perguntas import perguntas 
+
+
+# Função para para limpar o terminal em sistemas Windows e Unix/Linux
+def limpar_terminal():
+    os.system("cls" if os.name == "nt" else "clear")  
+
+
+
+# Funcção para exibir cada pergunta e suas opções
+def exibir_pergunta(questao, numero):
+    #  "--- Pergunta 1 de 10 ---"
+    print(f"--- Pergunta {numero} de {len(perguntas)} ---")
+    # Imprime a pergunta
+    print(f"\n{questao['pergunta']}\n")  
+    # percorre a lista de opções da pergunta
+    for opcao in questao["opcoes"]:
+        print(opcao)  # Imprime as opções de resposta
+
+
+
+def verificar_resposta(resposta_usuario, resposta_correta):
+    # Retorna True se a resposta estiver correta, caso contrário, retorna False
+    return resposta_usuario == resposta_correta 
+
+
+def comecar_jogo():
+    # iniciando a pontuação
+    pontuacao = 0  
+
+    # for para percorrer nossa lista perguntas
+    # enumerate é uma função em python que retorna o índice "0, 1, 2..." e o valor
+    # for cria duas variaveis, index para guardar o índice do enumerate e questao para guardar o valor do dicionário
+    for index, questao in enumerate(perguntas):
+        # Limpa o terminal antes de exibir cada pergunta
+        limpar_terminal()  
+        # chama a função de exibir a pergunta e suas opçoes passando a questao e o número da pergunta (index + 1)
+        exibir_pergunta(questao, index + 1)  
+
+        # verificação da resposta do usuario
+        while True:
+            # Lê a resposta do usuário, remove espaços e converte para maiúscula
+            resposta = input("\nDigite a sua resposta: ").strip().upper()  
+            # if in, verifica se a resposta esta dentro desta lista de opçoes esperadas
+            if resposta in ["A", "B", "C", "D"]:
+                break  # Sai do loop se a resposta for válida
+            else:
+                print("Por favor, digite A, B, C ou D.") 
+        
+
+        if verificar_resposta(resposta, questao["resposta"]):
+            print("✅ PARABENS, VOCÊ ACERTOU!\n")
+            pontuacao += 1  # Incrementa a pontuação em 1
+        else:
+            print(f"❌ RESPOSTA ERRADA!, A RESPOSTA CORRETA É: {questao['resposta']}")
+
+        time.sleep(2)  # Pausa por 2 segundos antes de mostrar a próxima pergunta
+
+    # Apos o quiz retorna a pontuação do jogador
+    return pontuacao  
+
+
+
+
+# AQUI COMEÇA NOSSO CODIGO PRINCIPAL
+# Inicia o jogo chamando a função comecar_jogo()
+# atri
+pontuacao = comecar_jogo()  
+
+
+limpar_terminal()
+
+print("=" * 30)
+print("--- FIM DE JOGO ---")
+print(f"Você acertou {pontuacao} de {len(perguntas)} perguntas")
+print("=" * 30)
+
+
+   
---
+++ perguntas.py
@@ -0,0 +1,60 @@
+# Lista Perguntas contendo dicionários com as perguntas, opções e respostas corretas
+perguntas = [
+    # dicionarios
+    {
+        # chaves e valores
+        "pergunta": "Qual palavra-chave é usada para criar uma função em Python?",
+        "opcoes": ["A) function", "B) create", "C) def", "D) func"],
+        "resposta": "C"
+    },
+    {
+        "pergunta": "Qual estrutura de dados utiliza chaves e valores?",
+        "opcoes": ["A) Lista", "B) Tupla", "C) String", "D) Dicionário"],
+        "resposta": "D"
+    },
+    {
+        "pergunta": "Qual operador é usado para comparar igualdade em Python?",
+        "opcoes": ["A) =", "B) ==", "C) !=", "D) ==="],
+        "resposta": "B"
+    },
+    {
+        "pergunta": "Qual destes tipos de dados é imutável?",
+        "opcoes": ["A) Lista", "B) Dicionário", "C) Tupla", "D) Conjunto"],
+        "resposta": "C"
+    },
+    {
+        "pergunta": "Qual comando exibe algo na tela em Python?",
+        "opcoes": ["A) show()", "B) display()", "C) print()", "D) output()"],
+        "resposta": "C"
+    },
+    {
+        "pergunta": "Qual laço é usado para percorrer uma lista?",
+        "opcoes": ["A) while", "B) if", "C) for", "D) switch"],
+        "resposta": "C"
+    },
+    {
+        "pergunta": "Qual método adiciona um elemento ao final de uma lista?",
+        "opcoes": ["A) add()", "B) append()", "C) insert()", "D) push()"],
+        "resposta": "B"
+    },
+    {
+        "pergunta": "O que significa HTML?",
+        "opcoes": [
+            "A) Hyper Text Markup Language",
+            "B) High Transfer Machine Language",
+            "C) Hyper Tool Machine Language",
+            "D) Home Text Markup Language"
+        ],
+        "resposta": "A"
+    },
+    {
+        "pergunta": "Qual linguagem é utilizada para estilizar páginas web?",
+        "opcoes": ["A) HTML", "B) Python", "C) CSS", "D) SQL"],
+        "resposta": "C"
+    },
+    {
+    "pergunta": "Qual estrutura de repetição executa enquanto uma condição for verdadeira?",
+    "opcoes": ["A) if", "B) for", "C) while", "D) def"],
+    "resposta": "C"
+    }
+]
